ae4b06dbfee83d42558cbde077282f1aad621d03
KoanJan <j.kehong@gmail.com>
2018-03-08 19:00:30 +0800

    WIP[BTree]

--- tree/b_tree/b_tree.go
+++ tree/b_tree/b_tree.go
@@ -26,6 +26,10 @@ func findInBTreeNode(node *treeNode, key int) (*treeNode, bool) {
 }
 
 func (tree *BTree) Insert(key int) {
+	if tree.node == nil {
+		tree.node = &treeNode{[]int{key}, nil, true, nil}
+		return
+	}
 	node, exist := findInBTreeNode(tree.node, key)
 	if exist {
 		return
@@ -46,8 +50,7 @@ func split(node *treeNode, t int) *treeNode {
 	upKey, leftKeys, rightKeys := node.keys[t], node.keys[0:t], node.keys[t+1:]
 	var leftChildren, rightChildren []*treeNode
 	if !node.leaf {
-		leftChildren = node.children[0 : t+1]
-		rightChildren = node.children[t+1:]
+		leftChildren, rightChildren = node.children[0:t+1], node.children[t+1:]
 	}
 	left := &treeNode{leftKeys, leftChildren, node.leaf, node.parent}
 	right := &treeNode{rightKeys, rightChildren, node.leaf, node.parent}
@@ -68,9 +71,61 @@ func split(node *treeNode, t int) *treeNode {
 	parent.children = append(parent.children, nil)
 	idx2 := ln
 	for idx2 > idx {
-		parent.keys[idx2] = parent.keys[idx2-1]
-		parent.children[idx2+1] = parent.children[idx2]
+		parent.keys[idx2], parent.children[idx2+1] = parent.keys[idx2-1], parent.children[idx2]
+		idx2 -= 1
 	}
 	parent.keys[idx2], parent.children[idx2], parent.children[idx+1] = upKey, left, right
 	return split(parent, t)
+}
+
+func (tree *BTree) Delete(key int) {
+	node, exist := findInBTreeNode(tree.node, key)
+	if exist {
+		tree.node = deleteKeyFromNode(node, key, tree.t)
+	}
+}
+
+// TODO finish it
+func deleteKeyFromNode(node *treeNode, key, t int) *treeNode {
+	//
+	idx := indexOfKey(node, key)
+	if node.parent == nil || len(node.keys) > t-1 {
+		if node.leaf {
+			deleteKey(node, key)
+			return node
+		} else {
+			if len(node.children[idx].keys) > t-1 {
+				// swap and recursively delete
+				node.keys[idx] = node.children[idx].keys[len(node.children[idx].keys)-1]
+				return deleteKeyFromNode(node.children[idx], node.keys[idx], t)
+			} else if len(node.children[idx+1].keys) > t-1 {
+				// swap and recursively delete
+				node.keys[idx] = node.children[idx+1].keys[0]
+				return deleteKeyFromNode(node.children[idx+1], node.keys[idx], t)
+			} else {
+				// combine
+				combine(node, idx, t)
+				return deleteKeyFromNode(node.children[idx], key, t)
+			}
+		}
+	} else {
+		// balance
+		pIdx := indexOfChild(node.parent, node)
+		if pIdx > 0 && len(node.parent.children[pIdx-1].keys) > t-1 {
+			// borrow from left sibling
+			borrowFromLeft(node.parent, pIdx)
+		} else if pIdx < len(node.parent.children)-1 && len(node.parent.children[pIdx+1].keys) > t-1 {
+			// borrow from right sibling
+			borrowFromRight(node.parent, pIdx)
+		} else if pIdx > 0 {
+			// combine with left sibling
+			combine(node.parent, pIdx-1, t)
+			node = node.parent.children[pIdx-1]
+		} else {
+			// combine with right sibling
+			combine(node.parent, pIdx, t)
+			node = node.parent.children[pIdx]
+		}
+		return deleteKeyFromNode(node, key, t)
+	}
 }
--- tree/b_tree/node.go
+++ tree/b_tree/node.go
@@ -21,6 +21,7 @@ func findInKeys(keys []int, key int) (int, bool) {
 	return idx, false
 }
 
+// // just insert the key and nothing will happen to children
 func insertKeyIntoLeafNode(node *treeNode, key int) int {
 	idx, ln := 0, len(node.keys)
 	node.keys = append(node.keys, nilKey)
@@ -31,4 +32,97 @@ func insertKeyIntoLeafNode(node *treeNode, key int) int {
 	}
 	node.keys[idx] = key
 	return idx
+}
+
+func indexOfKey(node *treeNode, key int) int {
+	for i := 0; i < len(node.keys); i++ {
+		if node.keys[i] == key {
+			return i
+		}
+	}
+	return -1
+}
+
+func indexOfChild(parent, child *treeNode) int {
+	for i := 0; i < len(parent.children); i++ {
+		if parent.children[i] == child {
+			return i
+		}
+	}
+	return -1
+}
+
+// just delete the key and nothing will happen to children
+func deleteKeyWithIndex(node *treeNode, index int) {
+	index += 1
+	ln := len(node.keys)
+	for index < ln {
+		node.keys[index-1] = node.keys[index]
+		index += 1
+	}
+	node.keys = node.keys[0 : ln-1]
+}
+
+// just delete the key and nothing will happen to children
+func deleteKey(node *treeNode, key int) {
+	idx := indexOfKey(node, key)
+	if idx < 0 {
+		return
+	}
+	deleteKeyWithIndex(node, idx)
+}
+
+// combine two children whose indices are 'index' and 'index+1'
+func combine(node *treeNode, index, t int) *treeNode {
+	if node.parent == nil || len(node.keys) > t-1 {
+		key := node.keys[index]
+		deleteKeyWithIndex(node, index)
+		left, right := node.children[index], node.children[index+1]
+		leftLn, rightLn := len(left.keys), len(right.keys)
+		newKeys := make([]int, leftLn+rightLn+1)
+		for i := 0; i < leftLn; i++ {
+			newKeys[i] = left.keys[i]
+		}
+		newKeys[leftLn] = key
+		for i := 0; i < rightLn; i++ {
+			newKeys[i+leftLn+1] = right.keys[i]
+		}
+		newChildren := make([]*treeNode, leftLn+1+rightLn+1)
+		for i := 0; i <= leftLn; i++ {
+			newChildren[i] = left.children[i]
+		}
+		for i := 0; i <= rightLn; i++ {
+			newChildren[i+leftLn+1] = right.children[i]
+		}
+		newNode := &treeNode{newKeys, newChildren, left.leaf, node}
+		node.children[index] = newNode
+		ln := len(node.children)
+		for i := index + 1; i < ln-1; i++ {
+			node.children[i] = node.children[i+1]
+		}
+		node.children = node.children[0 : ln-1]
+	} else {
+		// TODO node's length of keys may be less than t-1
+	}
+}
+
+// index: the index of node in parent's children
+func borrowFromLeft(parent *treeNode, index int) {
+	left, node := parent.children[index-1], parent.children[index]
+	node.keys = append([]int{parent.keys[index-1]}, node.keys...)
+	leftLn := len(left.keys)
+	node.children = append([]*treeNode{left.children[leftLn]}, node.children...)
+	parent.keys[index-1] = left.keys[leftLn-1]
+	left.keys = left.keys[0 : leftLn-1]
+	left.children = left.children[0:leftLn]
+}
+
+// index: the index of node in parent's children
+func borrowFromRight(parent *treeNode, index int) {
+	right, node := parent.children[index+1], parent.children[index]
+	node.keys = append(node.keys, parent.keys[index])
+	node.children = append(node.children, right.children[0])
+	parent.keys[index] = right.keys[0]
+	right.keys = right.keys[1:]
+	right.children = right.children[1:]
 }
