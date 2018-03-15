4e5b1696c45c2c44b092dac82d093c604c4bd555
KoanJan <j.kehong@gmail.com>
2018-03-15 16:55:00 +0800

    fix: better BellmanFord

--- graph/lib/bellman_ford.go
+++ graph/lib/bellman_ford.go
@@ -6,14 +6,17 @@ func BellmanFord(graph *core.Graph, vertex *core.Vertex) (map[*core.Vertex]int, 
 	disMap := make(map[*core.Vertex]int)
 	n := len(graph.Vertices)
 	// n-1 times
-	for i := 1; i < n; i++ {
+	relaxed := true
+	for i := 1; relaxed && i < n; i++ {
 		// iterate all the edges
+		relaxed = false
 		for s, edge := range graph.Edges {
 			for t, w := range edge {
 				if _, existed := disMap[s]; existed {
 					_, newVertex := disMap[t]
 					if newVertex || disMap[t] > disMap[s]+w {
 						disMap[t] = disMap[s] + w
+						relaxed = true
 					}
 				}
 			}
